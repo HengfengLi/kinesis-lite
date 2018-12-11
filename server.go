af8ec9b6be3c8eb3acf4a767eb597654b8122706
Hengfeng Li <hengf.li@gmail.com>
2018-12-11 22:52:44 +1100

    Try to use goleveldb and play around with json.

--- server.go
+++ server.go
@@ -6,12 +6,36 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 // CreateStreamRequest represents a request to create a stream
 type CreateStreamRequest struct {
 	StreamName string
 	ShardCount int
+}
+
+type EnhancedMonitoringData struct {
+	ShardLevelMetrics []string
+}
+
+type ShardData struct {
+	ShardId string
+}
+
+type StreamData struct {
+	RetentionPeriodHours    string
+	EnhancedMonitoring      []EnhancedMonitoringData
+	EncryptionType          string
+	HasMoreShards           bool
+	Shards                  []ShardData
+	StreamARN               string
+	StreamName              string
+	StreamStatus            string
+	StreamCreationTimestamp int
+	_seqIx                  []int    // Hidden data, remove when returning
+	_tags                   []string // Hidden data, remove when returning
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -42,6 +66,32 @@ func createStream(data []byte) {
 	json.Unmarshal(data, &req)
 	fmt.Printf("StreamName: %s\n", req.StreamName)
 	fmt.Printf("ShardCount: %d\n", req.ShardCount)
+
+	db := initDB()
+	defer db.Close()
+
+	stream := StreamData{StreamName: req.StreamName}
+	streamBytes, _ := json.Marshal(&stream)
+
+	err := db.Put([]byte(req.StreamName), streamBytes, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	val, err := db.Get([]byte(req.StreamName), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(string(val))
+}
+
+func initDB() *leveldb.DB {
+	db, err := leveldb.OpenFile("/tmp/kinesis-lite.db", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
 
 func main() {
